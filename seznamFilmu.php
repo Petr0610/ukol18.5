9a96e1c4c6a10fa0fff6ae54845e33410f91fdc7
Petr Vavrik <kerny9999@seznam.cz>
2021-05-18 12:58:08 +0200

    Zobrazení titulů jednotlive na radku

--- seznamFilmu.php
+++ seznamFilmu.php
@@ -1,6 +1,11 @@
 <?php
+$filepath = 'https://raw.githubusercontent.com/algolia/examples/master/demo-personalization/data.json';
+$json_string = file_get_contents($filepath);
+$json = json_decode($json_string, false);
 
-
-
+foreach ($json as $nazev) {
+ echo ($nazev->title . PHP_EOL);
+ 
+}
 
 
